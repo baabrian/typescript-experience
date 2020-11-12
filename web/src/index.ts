5c10ba66a044949e1bf21923c6f22f4457a9879b
Brian Nguyen <brianlnguyen12@gmail.com>
2020-11-11 23:18:09 -0800

    typescript

--- web/src/index.ts
+++ web/src/index.ts
@@ -5,6 +5,7 @@ import { Collection } from './models/Collection';
 
 const user = User.buildUser({ name: 'Name', age: 20 });
 const root = document.getElementById('root');
+
 if (root) {
   const userEdit = new UserEdit(root, user);
   userEdit.render();

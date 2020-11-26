fd1d5a43890f0b6ef679895cdef687fc2ed31392
Brian Nguyen <brianlnguyen12@gmail.com>
2020-11-25 21:51:39 -0800

    typescript

--- project-three/src/MatchResult.ts
+++ project-three/src/MatchResult.ts
@@ -1,4 +1,3 @@
-// enum - enumeration
 export enum MatchResult {
   HomeWin = 'H',
   AwayWin = 'A',
--- server/src/routes/loginRoutes.ts
+++ server/src/routes/loginRoutes.ts
@@ -7,26 +7,4 @@ interface RequestWithBody extends Request {
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
-  if (req.session && req.session.loggedIn) {
-    res.send(`
-    <div>
-      <div>You are logged in</div>
-      <a href="/logout">Logout</a>
-    </div>
-    `);
-  } else {
-    res.send(`
-    <div>
-      <div>You are not logged in</div>
-      <a href="/auth/login">Login</a>
-    </div>
-    `);
-  }
-});
-
-router.get('/protected', requireAuth, (req: RequestWithBody, res: Response) => {
-  res.send('Welcome to proteected Route.Your Password and email information');
-});
-
 export { router };
